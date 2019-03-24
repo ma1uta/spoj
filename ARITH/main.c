2b33cfcd09c0303d76f044ab6a2187771232473f
Anatoly Sablin <sablintolya@gmail.com>
2019-03-24 20:13:52 +0300

    ARITH. Multiplicate.

--- ARITH/main.c
+++ ARITH/main.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define MAX_DIGIT 501
 #define MAX_EXPRESSION 1001
 #define MAX(x,y) ((x) > (y) ? (x) : (y))
+#define MIN(x,y) ((x) < (y) ? (x) : (y))
 
 struct expression_t {
     char arg1[MAX_DIGIT];
     char arg2[MAX_DIGIT];
     char operator;
-};
-
-struct result {
-    struct expression_t* exp;
+    char gap_0[5]; // not used
+    size_t len1;
+    size_t len2;
     size_t res_count;
     char** res;
 };
 
 void calc(const char* input);
 
-int main()
-{
+int main() {
     int t;
     scanf("%d\n", &t);
 
@@ -34,188 +34,232 @@ int main()
     return 0;
 }
 
-void plus(struct result* res);
-void minus(struct result* res);
-void mult(struct result* res);
-void print(const struct result* res);
+void plus(struct expression_t* exp);
+void minus(struct expression_t* exp);
+void mult(struct expression_t* exp);
+void print(const struct expression_t* exp);
 
-void calc(const char* input)
-{
+void calc(const char* input) {
     struct expression_t exp;
 
-    size_t i = 0;
-    for (;input[i] != '+' && input[i] != '-' && input[i] != '*'; i++)
-    {
-        exp.arg1[i] = input[i];
+    size_t i;
+    size_t j;
+    size_t len = strlen(input);
+    for (j = 0, i = len - 1; input[i] != '+' && input[i] != '-' && input[i] != '*'; i--, j++) {
+        exp.arg2[j] = input[i];
     }
-    exp.arg1[i] = '\0';
+    exp.arg1[j + 1] = '\0';
+    exp.len2 = j;
     exp.operator = input[i];
-    size_t j = ++i;
-    for (; input[i] != '\0'; i++)
-    {
-        exp.arg2[i - j] = input[i];
+    for (j = 0; j < i; j++) {
+        exp.arg1[j] = input[i - j - 1];
     }
-    exp.arg2[i - j] = '\0';
+    exp.arg1[j] = '\0';
+    exp.len1 = j;
 
-    struct result* res = malloc(sizeof (struct result));
-    res->exp = &exp;
-    switch (exp.operator)
-    {
+    switch (exp.operator) {
     case '+':
-        plus(res);
+        plus(&exp);
         break;
     case '-':
-        minus(res);
+        minus(&exp);
         break;
     case '*':
-        mult(res);
+        mult(&exp);
         break;
     default:
         return;
     }
-    print(res);
-    for (size_t i = 0; i < res->res_count; i++)
-    {
-        free(res->res[i]);
+    print(&exp);
+    for (size_t i = 0; i < exp.res_count; i++) {
+        free(exp.res[i]);
     }
-    free(res->res);
-    free(res);
+    free(exp.res);
 }
 
-void plus(struct result* res)
-{
-    size_t len1 = strlen(res->exp->arg1);
-    size_t len2 = strlen(res->exp->arg2);
-    size_t len = MAX(len1, len2);
-    res->res_count = 1;
-    res->res = malloc(sizeof (char*));
+void plus(struct expression_t* exp) {
+    size_t len = MAX(exp->len1, exp->len2);
+    exp->res_count = 1;
+    exp->res = malloc(sizeof (char*));
 
-    char* inv_res = malloc(sizeof (char) * (len + 2));
+    exp->res[0] = malloc(sizeof (char) * (len + 2));
     size_t shift = 0;
     size_t i = 0;
-    while (i < len1 || i < len2)
-    {
-        char c1 = (i < len1) ? res->exp->arg1[len1 - i - 1] : '0';
-        char c2 = (i < len2) ? res->exp->arg2[len2 - i - 1] : '0';
+    while (i < exp->len1 || i < exp->len2) {
+        char c1 = (i < exp->len1) ? exp->arg1[i] : '0';
+        char c2 = (i < exp->len2) ? exp->arg2[i] : '0';
         unsigned long d1 = (unsigned long) (c1 - '0');
         unsigned long d2 = (unsigned long) (c2 - '0');
         char sum_str[3];
         unsigned long sum = d1 + d2 + shift;
         snprintf(sum_str, 3, "%ld", (d1 + d2 + shift));
-        if (sum > 9)
-        {
-            inv_res[i] = sum_str[1];
+        if (sum > 9) {
+            exp->res[0][i] = sum_str[1];
             shift = 1;
-        }
-        else
-        {
-            inv_res[i] = sum_str[0];
+        } else {
+            exp->res[0][i] = sum_str[0];
             shift = 0;
         }
         i++;
     }
-    if (shift)
-    {
-        inv_res[i] = '1';
+    if (shift) {
+        exp->res[0][i++] = '1';
     }
-    res->res[0] = malloc(sizeof (char) * (len + shift));
-    for (size_t j = 0; j < len + shift; j++)
-    {
-        res->res[0][j] = inv_res[len + shift - j - 1];
-    }
-    res->res[0][len + shift] = '\0';
-    free(inv_res);
+    exp->res[0][i+1] = '\0';
 }
 
-void minus(struct result* res)
-{
-    size_t len1 = strlen(res->exp->arg1);
-    size_t len2 = strlen(res->exp->arg2);
-    size_t len = MAX(len1, len2);
-    res->res_count = 1;
-    res->res = malloc(sizeof (char*));
+void minus(struct expression_t* exp) {
+    size_t len = MAX(exp->len1, exp->len2);
+    exp->res_count = 1;
+    exp->res = malloc(sizeof (char*));
+    exp->res[0] = malloc(sizeof (char) * (len + 1));
 
-    res->res[0] = malloc(sizeof (char) * (len + 1));
-
-    size_t shift = 0;
+    long shift = 0;
     size_t i = 0;
-    while (i < len1)
-    {
-        char c1 = (i < len1) ? res->exp->arg1[len1 - i - 1] : '0';
-        char c2 = (i < len2) ? res->exp->arg2[len2 - i - 1] : '0';
-        unsigned long d1 = (unsigned long) (c1 - '0');
-        unsigned long d2 = (unsigned long) (c2 - '0');
-        char sum_str[3];
-        long sum = d1 - d2 - shift;
-        snprintf(sum_str, 3, "%ld", (d1 + d2 + shift));
-        if (sum > 9)
-        {
-            inv_res[i] = sum_str[1];
+    while (i < exp->len1) {
+        char c1 = (i < exp->len1) ? exp->arg1[i] : '0';
+        char c2 = (i < exp->len2) ? exp->arg2[i] : '0';
+        long d1 = (long) (c1 - '0');
+        long d2 = (long) (c2 - '0');
+        char sum_str[2];
+        long sub = d1 - d2 - shift;
+        if (sub < 0) {
+            sub += 10;
             shift = 1;
-        }
-        else
-        {
-            inv_res[i] = sum_str[0];
+        } else {
             shift = 0;
         }
+        snprintf(sum_str, 2, "%ld", sub);
+        exp->res[0][i] = sum_str[0];
         i++;
     }
-    if (shift)
-    {
-        inv_res[i] = '1';
+    for (size_t j = 0; j < i; j++) {
+        if (exp->res[0][i - j - 1] == '0') {
+            exp->res[0][i - j - 1] = '\0';
+        }
     }
-    res->res[0] = malloc(sizeof (char) * (len + shift));
-    for (size_t j = 0; j < len + shift; j++)
-    {
-        res->res[0][j] = inv_res[len + shift - j - 1];
-    }
-    res->res[0][len + shift] = '\0';
-    free(inv_res);
 }
 
-void mult(struct result* res)
-{
+void mult(struct expression_t* exp) {
+    unsigned long mem = 0;
+    exp->res_count = exp->len2 + 1;
+    exp->res = malloc(sizeof (char*) * exp->res_count);
+    for (unsigned long i = 0; i < exp->len2; i++) {
+        unsigned long m2 = (unsigned long) (exp->arg2[i] - '0');
+        if (m2 == 0) {
+            exp->res[i] = malloc(sizeof (char) * 2);
+            exp->res[i][0] = '0';
+            exp->res[i][1] = '\0';
+        } else {
+            exp->res[i] = malloc(sizeof (char) * (exp->len1 + 2));
+            size_t j;
+            for (j = 0; j < exp->len1; j++) {
+                unsigned long m1 = (unsigned long) (exp->arg1[j] - '0');
+                unsigned long mult = m1 * m2 + mem;
+                mem = mult / 10;
+                unsigned long res = mult % 10;
+                exp->res[i][j] = (char) res + '0';
+            }
+            while (mem != 0) {
+                unsigned long res = mem % 10;
+                exp->res[i][j++] = (char) res + '0';
+                mem = mem / 10;
+            }
+            exp->res[i][j] = '\0';
+        }
+    }
+    unsigned long max = 0;
+    size_t* lengths = malloc(sizeof (size_t) * (exp->res_count - 1));
+    for (size_t i = 0; i < exp->res_count - 1; i++) {
+        lengths[i] = strlen(exp->res[i]);
+        max = MAX(max, lengths[i] + i);
+    }
+    exp->res[exp->res_count - 1] = malloc(sizeof (char) * (max + 2));
+    unsigned long offset = 0;
+    for (size_t i = 0; i < max; i++) {
+        unsigned long sum = 0;
+        size_t min = MIN(i, exp->res_count - 1);
+        for (size_t j = 0; j <= min; j++) {
+            if (lengths[j] + j > i) {
+                sum += (unsigned long) (exp->res[j][i - j] - '0') + offset;
+                if (sum > 9) {
+                    offset = sum / 10;
+                    sum %= 10;
+                }
+            }
+        }
+        exp->res[exp->res_count - 1][i] = (char) sum + '0';
+    }
+    if (offset > 0) {
+        exp->res[exp->res_count - 1][max] = (char) offset + '0';
+        exp->res[exp->res_count - 1][max + 1] = '\0';
+    } else {
+        exp->res[exp->res_count - 1][max] = '\0';
+    }
+    for (size_t i = 0; i < max - 1; i++) {
+        if (exp->res[exp->res_count - 1][max - i - 1] == '0') {
+            exp->res[exp->res_count - 1][max - i - 1] = '\0';
+        } else {
+            break;
+        }
+    }
+    free(lengths);
 }
 
-void print(const struct result* res)
-{
-    size_t len1 = strlen(res->exp->arg1);
-    size_t len2 = strlen(res->exp->arg2);
-    size_t max = MAX(len1, len2 + 1);
-    for (size_t i = 0; i < res->res_count; i++)
-    {
-        max = MAX(max, strlen(res->res[i]));
+void print(const struct expression_t* exp) {
+    size_t max = MAX(exp->len1, exp->len2 + 1);
+    for (size_t i = 0; i < exp->res_count; i++) {
+        max = MAX(max, strlen(exp->res[i]));
     }
 
-    for (size_t i = 0; i < max - len1; i++)
-    {
+    for (size_t i = 0; i < max - exp->len1; i++) {
         fputc(' ', stdout);
     }
-    fputs(res->exp->arg1, stdout);
+    for (size_t i = 0; i < exp->len1; i++) {
+        fputc(exp->arg1[exp->len1 - i - 1], stdout);
+    }
     fputc('\n', stdout);
 
-    for (size_t i = 0; i < max - len2 - 1; i++)
-    {
+    for (size_t i = 0; i < max - exp->len2 - 1; i++) {
         fputc(' ', stdout);
     }
-    fputc(res->exp->operator, stdout);
-    fputs(res->exp->arg2, stdout);
-    fputc('\n', stdout);
-
-    for (size_t i = 0; i < max; i++)
-    {
-        fputc('-', stdout);
+    fputc(exp->operator, stdout);
+    for (size_t i = 0; i < exp->len2; i++) {
+        fputc(exp->arg2[exp->len2 - i - 1], stdout);
     }
     fputc('\n', stdout);
 
-    for (size_t i = 0; i < res->res_count; i++)
-    {
-        size_t len = strlen(res->res[i]);
-        for (size_t j = 0; j < max - len - i; j++)
-        {
+    for (size_t i = 0; i < max; i++) {
+        fputc(i < MIN(max - exp->len2 - 1, max - exp->len1) ? ' ' : '-', stdout);
+    }
+    fputc('\n', stdout);
+
+    for (size_t i = 0; i < exp->res_count - 1; i++) {
+        size_t len = strlen(exp->res[i]);
+        for (size_t j = 0; j < max - len - i; j++) {
             fputc(' ', stdout);
         }
-        fputs(res->res[i], stdout);
+        for (size_t j = 0; j < len; j++) {
+            fputc(exp->res[i][len - j - 1], stdout);
+        }
+        fputc('\n', stdout);
+    }
+    if (exp->operator == '*' && exp->res_count > 2) {
+        for (size_t i = 0; i < max; i++) {
+            fputc('-', stdout);
+        }
+        fputc('\n', stdout);
+    }
+    if (exp->operator != '*' || exp->res_count > 2) {
+        size_t len = strlen(exp->res[exp->res_count - 1]);
+        if (len < max) {
+            for (size_t i = 0; i < max - len; i++) {
+                fputc(' ', stdout);
+            }
+        }
+        for (size_t i = 0; i < len; i++) {
+            fputc(exp->res[exp->res_count - 1][len - i -1], stdout);
+        }
         fputc('\n', stdout);
     }
 }
